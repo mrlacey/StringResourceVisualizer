0ffc5b07891c3fd4282aecd93a1464d2792a1596
Matt Lacey <matt@mrlacey.co.uk>
2018-12-22 19:27:08 +0000

    remove out-of-date TODO for closed issue

--- src/MyLineTransformSource.cs
+++ src/MyLineTransformSource.cs
@@ -24,7 +24,6 @@ namespace StringResourceVisualizer
             int lineNumber = line.Snapshot.GetLineFromPosition(line.Start.Position).LineNumber;
             LineTransform lineTransform;
 
-            // TODO: Don't show if line is collapsed. Issue #3
             if (this.manager.DisplayedTextBlocks.ContainsKey(lineNumber))
             {
                 var defaultTopSpace = line.DefaultLineTransform.TopSpace;
