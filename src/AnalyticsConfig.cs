245775bbaca7edbc8d75f874875a6a518f19606e
Matt Lacey <matt@mrlacey.co.uk>
2024-11-18 11:28:33 +0000

    add basic usage analytics/telemetry

---
+++ src/AnalyticsConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace StringResourceVisualizer
+{
+	public static class AnalyticsConfig
+	{
+		public static string TelemetryConnectionString { get; set; } = "";
+	}
+}
--- src/StringResVizPackage.cs
+++ src/StringResVizPackage.cs
@@ -12,6 +12,8 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 using EnvDTE;
 using Microsoft;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.PlatformUI;
@@ -98,9 +100,44 @@ namespace StringResourceVisualizer
             await SponsorRequestHelper.CheckIfNeedToShowAsync();
 
             Instance = this;
-        }
 
-        private async Task<bool> IsSolutionLoadedAsync(CancellationToken cancellationToken)
+			await TrackBasicUsageAnalyticsAsync();
+		}
+
+		private static async Task TrackBasicUsageAnalyticsAsync()
+		{
+			try
+			{
+#if !DEBUG
+			if (string.IsNullOrWhiteSpace(AnalyticsConfig.TelemetryConnectionString))
+			{
+				return;
+			}
+
+			var config = new TelemetryConfiguration
+			{
+				ConnectionString = AnalyticsConfig.TelemetryConnectionString,
+			};
+
+			var client = new TelemetryClient(config);
+
+			var properties = new Dictionary<string, string>
+				{
+					{ "VsixVersion", Vsix.Version },
+					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+				};
+
+			client.TrackEvent(Vsix.Name, properties);
+#endif
+			}
+			catch (Exception exc)
+			{
+				System.Diagnostics.Debug.WriteLine(exc);
+				await OutputPane.Instance.WriteAsync("Error tracking usage analytics: " + exc.Message);
+			}
+		}
+
+		private async Task<bool> IsSolutionLoadedAsync(CancellationToken cancellationToken)
         {
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
