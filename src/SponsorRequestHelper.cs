8480491c8e115680e7bb5c58353046e9a079bb66
Matt Lacey <matt@mrlacey.co.uk>
2025-03-19 18:22:27 +0000

    update sponsor request message

--- src/SponsorRequestHelper.cs
+++ src/SponsorRequestHelper.cs
@@ -36,16 +36,29 @@ namespace StringResourceVisualizer
         }
 
         private static async Task ShowPromptForSponsorshipAsync()
-        {
-            await OutputPane.Instance.WriteAsync("Sorry to interrupt. I know your time is busy, presumably that's why you installed this extension (String Resource Visualizer).");
-            await OutputPane.Instance.WriteAsync("I'm happy that the extensions I've created have been able to help you and many others");
-            await OutputPane.Instance.WriteAsync("but I also need to make a living, and limited paid work over the last few years has been a challenge. :(");
-            await OutputPane.Instance.WriteAsync(string.Empty);
-            await OutputPane.Instance.WriteAsync("Show your support by making a one-off or recurring donation at https://github.com/sponsors/mrlacey");
-            await OutputPane.Instance.WriteAsync(string.Empty);
-            await OutputPane.Instance.WriteAsync("If you become a sponsor, I'll tell you how to hide this message too. ;)");
-            await OutputPane.Instance.WriteAsync(string.Empty);
-            await OutputPane.Instance.ActivateAsync();
-        }
+		{
+			await OutputPane.Instance.WriteAsync("********************************************************************************************************");
+			await OutputPane.Instance.WriteAsync("This is a free extension that is made possible thanks to the kind and generous donations of:");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("Daniel, James, Mike, Bill, unicorns39283, Martin, Richard, Alan, Howard, Mike, Dave, Joe, ");
+			await OutputPane.Instance.WriteAsync("Alvin, Anders, Melvyn, Nik, Kevin, Richard, Orien, Shmueli, Gabriel, Martin, Neil, Daniel, ");
+			await OutputPane.Instance.WriteAsync("Victor, Uno, Paula, Tom, Nick, Niki, chasingcode, luatnt, holeow, logarrhythmic, kokolorix, ");
+			await OutputPane.Instance.WriteAsync("Guiorgy, Jessé, pharmacyhalo, MXM-7, atexinspect, João, hals1010, WTD-leachA, andermikael, ");
+			await OutputPane.Instance.WriteAsync("spudwa, Cleroth, relentless-dev-purchases & 20+ more");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("Join them to show you appreciation and ensure future maintenance and development by becoming a sponsor.");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("Go to https://github.com/sponsors/mrlacey");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("Any amount, as either a one-off or on a monthly basis, is appreciated more than you can imagine.");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("I'll also tell you how to hide this message too.  ;)");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("If you can't afford to support financially, you can always");
+			await OutputPane.Instance.WriteAsync("leave a positive review at https://marketplace.visualstudio.com/items?itemName=MattLaceyLtd.StringResourceVisualizer&ssr=false#review-details");
+			await OutputPane.Instance.WriteAsync("");
+			await OutputPane.Instance.WriteAsync("********************************************************************************************************");
+		}
     }
 }
